9186cc741aff1727d33fa0871cb548160ba31091
Parsher <26663229+parsher@users.noreply.github.com>
2021-04-15 11:17:35 +0900

    client.ts
    hot reload 대응

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from './client';
 
 /* async function select() {
   // ... you will write your Prisma Client queries here
